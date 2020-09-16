874634c3f641e64515c3e9ea58223d6323e8b7b6
GajalakshmiRajkumar <aarthiranjan05@gmail.com>
2020-09-16 00:36:44 -0700

    push added

---
+++ src/empdetails/EmployeeData.java
@@ -0,0 +1,55 @@
+package empdetails;
+
+public class EmployeeData {
+
+public void empId() { 
+System.out.println("Employee id:12345");
+}
+
+public void empName() {
+System.out.println("Employee name:Gajalakshmi");
+}
+
+public void empDob() {
+System.out.println("Employee dob:08-11-1990");
+}
+
+public void empPhone() {
+System.out.println("Employee phone number:9790224018");
+}
+
+public void empEmailid() {
+System.out.println("Employee email:aarthiranjan05@gmail.com");
+}
+
+public void empAddress() {
+System.out.println("Employee address: Guduvanchery");
+}
+
+public static void main(String[] args) {
+
+
+
+	EmployeeData obj= new EmployeeData();
+			
+	
+	obj.empId();
+	
+	obj.empName();
+	
+	obj.empDob();
+	
+	obj.empPhone();
+	
+	obj.empEmailid();
+	
+	obj.empAddress();
+	
+}
+
+}
+		
+	
+	
+
+
